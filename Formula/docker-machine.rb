21775fd65f804b6882ad4a08ed2ffb4b19f61eac
Dan Lipsitt <danlipsitt@gmail.com>
2015-02-03 19:01:14 -0800

    Explicit version because of the way '-rc2' is handled.

--- Formula/docker-machine.rb
+++ Formula/docker-machine.rb
@@ -5,6 +5,7 @@ class DockerMachine < Formula
 
   devel do
     url "https://github.com/docker/machine/archive/v0.1.0-rc2.tar.gz"
+    version "0.1.0_rc2"
     sha1 "5251d959f6ff1ee6a7e7fb0d7b23aeb6152d7d3c"
   end
 
